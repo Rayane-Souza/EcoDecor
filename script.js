ede177bbcf511b3e0c22fc68c11becc2ed1896a3
Rayane Souza <130245094+Rayane-Souza@users.noreply.github.com>
2024-06-09 16:06:38 -0300

    cronometro-update

---
+++ script.js
@@ -0,0 +1,25 @@
+function updateClock() {
+
+    var endTime = new Date(2024, 5, 30, 18, 0, 0); 
+    
+    var now = new Date();
+    var timeDiff = endTime - now;
+    
+    if (timeDiff > 0) {
+        var hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+        var timeString = 
+            (hours > 0 ? hours + "h " : "") + 
+            (minutes > 0 ? minutes + "m " : "") + 
+            seconds + "s";
+            
+        document.getElementById("clock").innerHTML = timeString;
+    } else {
+        document.getElementById("clock").innerHTML = "00h 00m 00s";
+    }
+}
+
+setInterval(updateClock, 1000);
+updateClock();
