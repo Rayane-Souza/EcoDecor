beca8a86f571fc199edadbeeb82a5aa9568022b8
Beatriz Silva <annbia24@gmail.com>
2024-06-11 22:55:24 -0300

    login-changes

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-function mostrarSenha(){
-    var inputPass = document.getElementById('senha');
-    var btnMostrarPass = document.getElementById('btn-senha');
+function mostrarSenha() {
+  var inputPass = document.getElementById("senha");
+  var btnMostrarPass = document.getElementById("btn-senha");
 
-    if(inputPass.type === 'password'){
-        inputPass.setAttribute('type', 'text');
-        btnMostrarPass.classList.replace('fa-eye', 'fa-eye-slash');
-    }
-    else {
-        inputPass.setAttribute('type', 'password');
-        btnMostrarPass.classList.replace('fa-eye-slash', 'fa-eye');
-    }
+  if (inputPass.type === "password") {
+    inputPass.setAttribute("type", "text");
+    btnMostrarPass.classList.replace("fa-eye", "fa-eye-slash");
+  } else {
+    inputPass.setAttribute("type", "password");
+    btnMostrarPass.classList.replace("fa-eye-slash", "fa-eye");
+  }
 }
